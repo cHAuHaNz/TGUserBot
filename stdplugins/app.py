3433149dd5c975ac700e0a655ae001a72db94b21
cHAuHaNz <amanurfdanga@gmail.com>
2019-09-28 02:10:31 +0530

    .App Improved

--- stdplugins/app.py
+++ stdplugins/app.py
@@ -24,11 +24,13 @@ async def imdb(e):
         app_rating = results[0].findNext('div', 'Vpfmgd').findNext('div', 'pf5lIe').find('div')['aria-label']
         app_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
         app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
-        app_details = app_name
-        app_details += "\nDeveloper : ["+app_dev+"]("+app_dev_link+")"
-        app_details += "\nRating : "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
-        app_details += "\n[View in Play Store]("+app_link+")"
-        app_details += "\n[]("+app_icon+")"
+        app_details = "<b>"+app_name+"</b>"
+        app_details += "\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
+        app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
+        app_details += "\n<code>Features :</code><a href='"+app_link+"'>View in Play Store</a>"
+        app_details += "\n<a href='"+app_icon+"'>&#8203;</a>')"
+        app_details += "\n\n<b>Download : </b> <a href='http://t.me/Xpl0iter'>Request_Here</a>"
+        app_details += "\n\n===> @Xpl0iter <==="
         await e.edit(app_details, link_preview = True)
     except IndexError:
         await e.edit("No result found in search. Please enter **Valid app name**")
