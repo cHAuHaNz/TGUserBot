724910c83d6fe6a723728d9f8ea810b6b43616c5
cHAuHaNz <amanurfdanga@gmail.com>
2019-09-29 21:20:15 +0530

    Fixed App

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -1,11 +1,11 @@
-""" Command: .porn """
+""" Command: .ping """
 
 from telethon import events
 from datetime import datetime
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="porn ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="ping ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
