af5e2f53dd274d8fc2cae818663dcfe545f1e7c0
cHAuHaNz <amanurfdanga@gmail.com>
2019-09-29 21:23:48 +0530

    Fixed Autores

--- stdplugins/autores.py
+++ stdplugins/autores.py
@@ -1,7 +1,7 @@
 """Auto-responder for #Request by `cHAuHaN`
 .autores"""
 
-#regex ([a-zA-Z0-9 ]+)( #([r]|[R])equest)
+#regex ([a-zA-Z0-9 ]+)( #([r]|[R])equest)($|[\n])
 
 import asyncio
 import io
@@ -12,7 +12,7 @@ from uniborg.util import admin_cmd
 @borg.on(admin_cmd(incoming=True))
 async def on_snip(event):
     name = event.raw_text
-    pattern = r"([a-zA-Z0-9 ]+)( #([r]|[R])equest)"
+    pattern = r"([a-zA-Z0-9 ]+)( #([r]|[R])equest)($|[\n])"
     if re.search(pattern, name, flags=re.IGNORECASE):
         message_id = event.message.id
         #if event.reply_to_msg_id:
