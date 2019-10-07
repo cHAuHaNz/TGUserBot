1bebb923a1ea5ac9972a93ce1bf813c54ca22439
cHAuHaNz <amanurfdanga@gmai.com>
2019-10-08 00:37:33 +0530

    Updated Autores

--- stdplugins/autores.py
+++ stdplugins/autores.py
@@ -22,39 +22,16 @@ async def on_snip(event):
             "Your request has been received, It will be fulfilled ASAP.\n`Please don't send a duplicate request within 3 days.`",
             reply_to=message_id
         )
-        await event.client.send_message(
+        """await event.client.send_message(
             -346103366,
             "New Request Received :- `"+name+"`",
             reply_to=message_id
-        )
-
-"""
-@borg.on(events.Raw())
-async def on_new_channel_message(event):
-    try:
-        if Config.PM_LOGGR_BOT_API_ID is None:
-            return
-        if tgbot is None:
-            return
-        # logger.info(event.stringify())
-        if isinstance(event, types.UpdateChannel):
-            channel_id = event.channel_id
-            message_id = 2
-            # someone added me to channel
-            # TODO: https://t.me/TelethonChat/153947
-            the_message = ""
-            the_message += "#MessageActionChatAddUser\n\n"
-            # the_message += f"[User](tg://user?id={added_by_user}): `{added_by_user}`\n"
-            the_message += f"[Private Link](https://t.me/c/{channel_id}/{message_id})\n"
-            await borg.send_message(
-                entity=Config.PM_LOGGR_BOT_API_ID,
-                message=the_message,
-                # reply_to=,
-                # parse_mode="html",
-                link_preview=False,
-                # file=message_media,
-                silent=True
+        )"""
+        msg = await event.message
+        if msg:
+            msg_o = await event.client.forward_messages(
+                entity=-346103366,
+                messages=msg,
+                from_peer=event.chat_id,
+                silent=False
             )
-    except Exception as e:
-        print(str(e))
-"""
