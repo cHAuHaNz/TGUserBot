577231c6cc7ee9a82eedf2b556db1eb0b07babee
cHAuHaNz <amanurfdanga@gmai.com>
2019-10-13 00:24:30 +0530

    Removed extra spacing

--- stdplugins/wupload.py
+++ stdplugins/wupload.py
@@ -1,9 +1,6 @@
 """Emoji
-
 Available Commands:
-
 .wupload
-
 """
 from telethon import events
 import asyncio
@@ -11,42 +8,27 @@ import asyncio
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 9
+    animation_interval = 1
     animation_ttl = range(0, 15)
     input_str = event.pattern_match.group(1)
     if input_str == "wupload":
         await event.edit(input_str)
         animation_chars = [
             "Uploading File From Telegram To Whatsapp...",
-
             "User Online: True\nTelegram API Access: True\nWhatsapp API Access: True\nRead Storage: True ",
-
             "DOWNLOADING STARTED... \n\n0% [░░░░░░░░░░░░░░░░░░░░]\n`Connecting To WhatsApp API...`\nETA: 0m, 20s",
-
             "DOWNLOADING... \n\n11.07% [██░░░░░░░░░░░░░░░░░░]\n\nETA: 0m, 18s",
-
             "DOWNLOADING... \n\n20.63% [███░░░░░░░░░░░░░░░░░]\n\nETA: 0m, 16s",    
-
             "File Downloaded, Uploading to cHAuHaN's WhatsApp Group... \n\n34.42% [█████░░░░░░░░░░░░░░░]\n\nETA: 0m, 14s",
-
             "UPLOADING... \n\n42.17% [███████░░░░░░░░░░░░░]\n\nETA: 0m, 12s",
-
             "UPLOADING... \n\n55.30% [█████████░░░░░░░░░░░]\n\nETA: 0m, 10s",
-
             "UPLOADING... \n\n64.86% [███████████░░░░░░░░░]\n\nETA: 0m, 08s",
-
             "Uploaded to cHAuHaN's WhatsApp Group... \n\n74.02% [█████████████░░░░░░░]\n\nETA: 0m, 06s",
-
             "SPLITTING FILE IN WHATSAPP SUPPORTED SIZE & UPLOADING IT ... 86.21% [███████████████░░░░░]\n\nETA: 0m, 04s",
-
             "SPLITTING FILE IN WHATSAPP SUPPORTED SIZE & UPLOADING IT... 93.50% [█████████████████░░░]\n\nETA: 0m, 02s",
-
             "Uploading to cHAuHaN's WhatsApp Group... 100% [████████████████████]\n`Scanning file...`\nETA: 0m, 00s",
-
             "UPLOADING FILE TO WHATSAPP GROUP COMPLETED!\nFILE VERIFIED: ✅",
-
             "API TERMINATED UNTIL FURTHER USAGE..."
-
         ]
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
