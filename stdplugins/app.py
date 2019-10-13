f481bbf1975f35b69ec4107cb53a9f75e055f143
cHAuHaNz <amanurfdanga@gmai.com>
2019-10-14 00:10:07 +0530

    Fixed a bug when icon disappeared on editing the post.

--- stdplugins/app.py
+++ stdplugins/app.py
@@ -61,7 +61,7 @@ async def apkr(e):
         app_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
         app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
         app_details = "<a href='"+app_icon+"'>📲&#8203;</a>"
-        app_details += "\n<b>"+app_name+"</b>"
+        app_details += " <b>"+app_name+"</b>"
         app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
         app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
