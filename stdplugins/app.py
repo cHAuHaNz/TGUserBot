270f9ed6110b204cad78283d68dcdf110c9476fd
cHAuHaNz <amanurfdanga@gmai.com>
2019-10-14 00:05:57 +0530

    Fixed a bug when icon disappeared on editing the post.

--- stdplugins/app.py
+++ stdplugins/app.py
@@ -1,6 +1,8 @@
 """Fetch App Details from Playstore.
 .apk <app_name> to fetch app details.
-.apkr <app_name>  to fetch app details with Xpl0iter request link."""
+.apkr <app_name>  to fetch app details with Xpl0iter request link.
+  © @amnd33p
+"""
 import requests
 import bs4 
 import re
@@ -58,12 +60,12 @@ async def apkr(e):
         app_rating = results[0].findNext('div', 'Vpfmgd').findNext('div', 'pf5lIe').find('div')['aria-label']
         app_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
         app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
-        app_details = "<a href='"+app_icon+"'>&#8203;</a>"
+        app_details = "<a href='"+app_icon+"'>📲&#8203;</a>"
         app_details += "\n<b>"+app_name+"</b>"
         app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
         app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
-        app_details += "\n\n<b>Download : </b> <a href='http://t.me/Xpl0iter'>Request_Here</a>"
+        app_details += "\n\n<b>Download : </b> <a href='https://t.me/joinchat/JCu-H1NikiYDgNjpjPYd4A'>Request_Here</a>"
         app_details += "\n\n===> @Xpl0iter <==="
         await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
     except IndexError:
