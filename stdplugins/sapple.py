6823da08138f7e4d4432a5415da4bac8ab24c719
cHAuHaNz <amanurfdanga@gmai.com>
2019-10-14 12:49:25 +0530

    Testing sapple

--- stdplugins/sapple.py
+++ stdplugins/sapple.py
@@ -1,18 +1,36 @@
 from telethon import events
 import asyncio
 
-@borg.on(events.NewMessage(pattern=r"sapple", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
-    animation_chars = [
-                    " ‏‏‎ ",
-                    "        🍎        ",
-                    "      🍎 🍎       ",
-                    "    🍎 🍎 🍎      ",
-                    "  🍎 🍎 🍎 🍎     ",
-                    "🍎 🍎 🍎 🍎 🍎    "
-                ]
-    msg = ""
-    for s in animation_chars:
-        msg += s+"\n"
-        await event.edit(msg)
-        await asyncio.sleep(0.5)
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    if input_str == "sapple":
+        animation_chars = [
+                        " ‏‏‎ ",
+                        "        🍎        ",
+                        "      🍎 🍎       ",
+                        "    🍎 🍎 🍎      ",
+                        "  🍎 🍎 🍎 🍎     ",
+                        "🍎 🍎 🍎 🍎 🍎    "
+                    ]
+        msg = ""
+        for s in animation_chars:
+            msg += s+"\n"
+            await event.edit(msg)
+            await asyncio.sleep(0.5)
+    if input_str == "🍎":
+        animation_chars = [
+                        " ‏‏‎ ",
+                        "        🍎        ",
+                        "      🍎 🍎       ",
+                        "    🍎 🍎 🍎      ",
+                        "  🍎 🍎 🍎 🍎     ",
+                        "🍎 🍎 🍎 🍎 🍎    "
+                    ]
+        msg = ""
+        for s in animation_chars:
+            msg += s+"\n"
+            await event.edit(msg)
+            await asyncio.sleep(0.5)
