d6529682713069550043b346736cff2e1ae629b5
cHAuHaNz <chauhan@xploit.ml>
2019-12-13 11:59:25 +0530

    BugFix

--- stdplugins/sticker_rape.py
+++ stdplugins/sticker_rape.py
@@ -116,7 +116,7 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"This Sticker Is Raped by [" + me.first_name + "](http://t.me/"+ me.username +")! Rape can be found [here](t.me/addstickers/{packshortname}).")
+    await event.edit(f"This Sticker Is Raped by [" + me.first_name + "](http://t.me/"+ me.username +")! Rape can be found [here](t.me/addstickers/" + packshortname +").")
 
 
 @borg.on(admin_cmd("packinfo"))
